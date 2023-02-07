6258beb1afef9085442a4795a5c5e4c42433e560
Emmanuel Adeiza <38910805+EmmanuelAdeiza@users.noreply.github.com>
2023-02-07 18:00:35 +0100

    added text to dummy page

--- lib/modules/resources/views/resources.dart
+++ lib/modules/resources/views/resources.dart
@@ -1,4 +1,5 @@
 import 'package:faramove_test/core/asset_files.dart';
+import 'package:faramove_test/global/global.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -9,7 +10,16 @@ class Resources extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Lottie.asset(const AnimationAssets().empty2),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Lottie.asset(const AnimationAssets().empty2),
+            const YSpace(50),
+            const CustomText('Resources',
+                size: AppTextSize.title, weight: FontWeight.w600)
+          ],
+        ),
       ),
     );
   }
